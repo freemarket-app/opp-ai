97bd37d5805272738fbdb5c7cacef43007abcb30
ShiniYanada <ysthon@gmail.com>
2019-02-08 08:51:10 +0900

    Initial Commit

---
+++ opp-ai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  opp-ai
+//
+//  Created by 簗田信緯 on 2019/02/08.
+//  Copyright © 2019 opp-ai-organization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
